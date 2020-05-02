77a2ec7c5818f713ff2350db9c661fda1e883460
domo1124 <domo1124orfevre@gmail.com>
2020-05-02 18:22:52 +0900

    calc fix

--- functions/predict_result_output/main.py
+++ functions/predict_result_output/main.py
@@ -42,17 +42,17 @@ def predict_result_output(event, context):
         tan_h = race_data["tan_u"]
         tan_p = race_data["tan_p"]
         if no1 in tan_h:
-            tan_t += 0
+            tan_t += 1
             tan_sum += int(tan_p[tan_h.index(no1)])
         #連帯率の計算
         uren_h = race_data["umaren_u"]
         uren_p = race_data["umaren_p"]
         if no1 in uren_h:
-            ren1 += 0
+            ren1 += 1
         if no2 in uren_h:
-            ren2 += 0
+            ren2 += 1
         if no3 in uren_h:
-            ren3 += 0
+            ren3 += 1
         #馬連の買い目
         bet_set_ren = []
         for z in ren_main:
@@ -84,19 +84,19 @@ def predict_result_output(event, context):
         #馬連のチェック
         pay_count = 0          
         for i in range(0,len(fuku3_h),3):
-            ren_ture = '-'.join(uren_h[i:i+3])
+            ren_ture = '-'.join(fuku3_h[i:i+3])
             if ren_ture in bet_set_fuku3:
-                fuku3_sum += int(uren_p[pay_count])
+                fuku3_sum += int(fuku3_p[pay_count])
             pay_count += 1
     bet_sum_tan = pred_race_num*100
     bet_sum_ren = len(bet_set_ren)*100*pred_race_num
     bet_sum_fuku3 = len(bet_set_fuku3)*100*pred_race_num
     
-    tan_sum = round((tan_sum/pred_race_num),2)
+    tan_t = round(((tan_t/pred_race_num)*100),2)
     #連対数
-    ren1 = round((ren1/pred_race_num),2)
-    ren2 = round((ren2/pred_race_num),2)
-    ren3 = round((ren3/pred_race_num),2)
+    ren1 = round(((ren1/pred_race_num)*100),2)
+    ren2 = round(((ren2/pred_race_num)*100),2)
+    ren3 = round(((ren3/pred_race_num)*100),2)
     return_tan = round((tan_sum/bet_sum_tan)*100,2)
     return_ren = round((ren_sum/bet_sum_ren)*100,2)
     return_fuku3 = round((fuku3_sum/bet_sum_fuku3)*100,2)
@@ -111,7 +111,7 @@ def predict_result_output(event, context):
     馬連回収率　　{}%
     三連複回収率　{}%
     
-    """.format(race_date.strftime('%Y/%m/%d'),str(pred_race_num),str(tan_sum),str(ren1),str(ren2),str(ren3),str(return_tan),str(return_ren),str(return_fuku3))
+    """.format(race_date.strftime('%Y/%m/%d'),str(pred_race_num),str(tan_t),str(ren1),str(ren2),str(ren3),str(return_tan),str(return_ren),str(return_fuku3))
     topic_name = os.getenv('TOPIC_NAME')
     publisher = pubsub.PublisherClient()    
     topic_path = publisher.topic_path(project_id, topic_name)
